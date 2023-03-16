aa835023814eeca667433264685859dc2f42a871
Rohit Bajaj <108188800+Rohit-bajaj47@users.noreply.github.com>
2023-03-16 00:56:36 +0000

    modified:   server.js

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const { json } = require('express');
 const express = require('express'); 
-const res = require('express/lib/response');
-
 
 const app = express();
 
 app.use(express.json);
 app.use(express.urlencoded());
 
-
 let users = [
     {id: 1, name: 'rohit'},
     {id: 2, name: 'abhi'},
     {id: 3, name: 'navu'},
 ];
-
-
-
 
 app.listen(3000,()=>{
     console.log('The server have ears now on port 3000')
