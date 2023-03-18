e58c6cb79734ade222cfb7409520d92d1b890089
Rohit Bajaj <108188800+Rohit-bajaj47@users.noreply.github.com>
2023-03-18 17:11:11 +0000

    modified:   index.html 	modified:   server.js

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
+/*This code sets up an Express server that listens on port 3000. 
+It imports the 'path' library and initializes the Express app as a constant variable.
+The express.json() middleware function is used to parse incoming requests with JSON payloads. 
+The express.urlencoded() middleware function is used to parse incoming requests with urlencoded payloads.
+The app.get() method sets up a route for GET requests to the root URL ('/'). 
+When a GET request is made to the root URL, the server responds by sending the 'index.html' file using the res.sendFile() method.
+Finally, the app.listen() method starts the server and listens on port 3000. When the server is ready to accept connections, 
+it logs a message to the console.*/
+
+
 const { json } = require('express');
 const express = require('express'); 
+
+const path = require('path');
 const app = express(); 
 
-
-// Need to import Express libraries to read POST body data (e.g. req.body)
 app.use(express.json());             
 app.use(express.urlencoded()); 
 
-let users = [ 
-  { id: 1, name: 'John' }, 
-  { id: 2, name: 'Jane' }, 
-  { id: 3, name: 'Bob' }
-]; 
-
-
 app.get('/', (req, res) => { 
-  res.sendFile('index.html');
+  const options = {
+    root: path.join(__dirname)
+  };
+
+  const fileName = 'index.html';
+    res.sendFile(fileName, options);
 }); 
-
-
-
 
 app.listen(3000, () => { 
     console.log('Server is listening on port 3000'); 
 }); 
+
+//a simple express server to serve an index.html file
